b9bc8e788dc5a89319a950dd459810c55dc0f5f8
Marloschi <marlo.mitterlehner@gmx.at>
2023-01-10 15:05:06 +0100

    Abstract Class

--- src/at/marlo/games/strategypattern/CircleActor.java
+++ src/at/marlo/games/strategypattern/CircleActor.java
@@ -1,25 +1,17 @@
 package at.marlo.games.strategypattern;
 
-import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 
-public class CircleActor implements Actor {
-    private final MoveStrategy moveStrategy;
+public class CircleActor extends AbstractActor {
+
 
     public CircleActor(MoveStrategy moveStrategy) {
-        super();
-        this.moveStrategy = moveStrategy;
+        super(moveStrategy);
     }
-
 
     @Override
     public void render(Graphics graphics) {
 
         graphics.drawOval(this.moveStrategy.getX(), this.moveStrategy.getY(), 80, 80);
-    }
-
-    @Override
-    public void update(GameContainer gameContainer, int delta) {
-        this.moveStrategy.update(delta);
     }
 }
--- src/at/marlo/games/strategypattern/RectangleActor.java
+++ src/at/marlo/games/strategypattern/RectangleActor.java
@@ -1,25 +1,17 @@
 package at.marlo.games.strategypattern;
 
-import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 
-public class RectangleActor implements Actor {
-    private final MoveStrategy moveStrategy;
+public class RectangleActor extends AbstractActor {
 
     public RectangleActor(MoveStrategy moveStrategy) {
-        super();
-        this.moveStrategy = moveStrategy;
+        super(moveStrategy);
     }
-
 
     @Override
     public void render(Graphics graphics) {
 
         graphics.drawRect(this.moveStrategy.getX(), this.moveStrategy.getY(), 80, 80);
     }
-
-    @Override
-    public void update(GameContainer gameContainer, int delta) {
-        this.moveStrategy.update(delta);
-    }
 }
+
