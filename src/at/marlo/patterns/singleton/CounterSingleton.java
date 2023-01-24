ca7de07f891c50f7054be79327dc8de57dddee86
Marloschi <marlo.mitterlehner@gmx.at>
2023-01-24 14:22:28 +0100

    Singleton

---
+++ src/at/marlo/patterns/singleton/CounterSingleton.java
@@ -0,0 +1,27 @@
+package at.marlo.patterns.singleton;
+
+public class CounterSingleton {
+    private static CounterSingleton instance = null;
+    private int Counter = 0;
+
+
+    private CounterSingleton() {
+    }
+
+    public static CounterSingleton getInstance() {
+        if (instance == null) {
+            instance = new CounterSingleton();
+            return instance;
+        } else {
+            return instance;
+        }
+    }
+
+    public void increaseCounter() {
+        this.Counter++;
+    }
+
+    public int getCounter() {
+        return Counter;
+    }
+}
